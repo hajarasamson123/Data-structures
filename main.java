9852fd3c7ece2c951b664d41a57b8e02b56da2be
hajarasamson123 <132559216+hajarasamson123@users.noreply.github.com>
2024-06-11 06:44:53 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(10);
+        myStack.push(0);
+        myStack.push(45);
+        myStack.push(95);
+        myStack.push(68);
+        myStack.push(80);
+        myStack.pop(0);
+        myStack.pop(0);
+        myStack.printStack();
+    
+    }
+}
